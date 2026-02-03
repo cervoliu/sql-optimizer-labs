b17eb0dad898757db48cddc6630df2a8dddeefd0
cervol@MacbookAir <cervol@foxmail.com>
2026-02-03 14:32:17 +0800

    minimal plan cancel rules; pass test 6

--- src/plan.rs
+++ src/plan.rs
@@ -1,6 +1,6 @@
 //! Plan optimization rules.
 
-use std::collections::HashSet;
+use std::{collections::HashSet, vec};
 
 use super::*;
 use egg::rewrite as rw;
@@ -10,9 +10,43 @@ pub fn rules() -> Vec<Rewrite> {
     let mut rules = vec![];
     rules.extend(projection_pushdown_rules());
     rules.extend(join_rules());
-    // TODO: add rules
-    rules
+    rules.extend(cancel_rules());
+    rules.extend(merge_rules());
+    return rules
 }
+
+#[rustfmt::skip]
+pub fn cancel_rules() -> Vec<Rewrite> { vec![
+    rw!("limit-0";   "(limit 0 0 ?a)" => "(empty ?a)"),
+    rw!("limit-null"; "(limit null 0 ?a)" => "?a"),
+    rw!("order-null"; "(order (list) ?a)" => "?a"),
+    rw!("filter-true";  "(filter true ?a)" => "?a"),
+    rw!("filter-false"; "(filter false ?a)" => "(empty ?a)"),
+    rw!("inner-join-false"; 
+        "(join inner false ?a ?b)" => 
+        "(empty (join inner false ?a ?b))"
+    ),
+    rw!("proj-on-empty"; "(proj ?cols (empty ?a))" => "(empty ?cols)"),
+    rw!("filter-on-empty"; "(filter ?cond (empty ?a))" => "(empty ?a)"),
+    rw!("order-on-empty"; "(order ?ord (empty ?a))" => "(empty ?a)"),
+    rw!("limit-on-empty"; "(limit ?lim ?off (empty ?a))" => "(empty ?a)"),
+]}
+
+#[rustfmt::skip]
+pub fn merge_rules() -> Vec<Rewrite> { vec![
+    rw!("limit-order-topn";
+        "(limit ?lim ?off (order ?ord ?a))" => 
+        "(topn ?lim ?off ?ord ?a)"
+    ),
+    rw!("filter-merge";
+        "(filter ?cond1 (filter ?cond2 ?a))" => 
+        "(filter (and ?cond1 ?cond2) ?a)"
+    ),
+    rw!("proj-merge";
+        "(proj ?cols1 (proj ?cols2 ?a))" => 
+        "(proj ?cols1 ?a)"
+    ),
+]}
 
 #[rustfmt::skip]
 pub fn join_rules() -> Vec<Rewrite> { vec![
